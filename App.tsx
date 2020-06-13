da5ce4c038381d15a9547d633de922be01cee242
sugajski <maciej.sugajski@kiwijobs.pl>
2020-06-13 21:33:49 +0200

    Installed dependencies and configured Reactotron.

--- App.tsx
+++ App.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+if (__DEV__) {
+  import("./ReactotronConfig").then(() =>
+    console.log("Reactotron Configured")
+  );
+}
+
 export default function App() {
   return (
     <View style={styles.container}>
